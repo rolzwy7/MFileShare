e23b181ea5261121cf983ce7c564d3878facc692
Bartosz Nowakowski <rolzwy7@gmail.com>
2022-02-28 15:41:24 +0100

    Fix AES secret creation

--- class.mfsp.crypto-init.php
+++ class.mfsp.crypto-init.php
@@ -1,12 +1,27 @@
 <?php
 
+function generateUUID4Secret()
+{
+    return sprintf(
+        '%04x%04x-%04x-%04x-%04x-%04x%04x%04x',
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0x0fff) | 0x4000,
+        mt_rand(0, 0x3fff) | 0x8000,
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff),
+        mt_rand(0, 0xffff)
+    );
+}
+
 if (is_file(MFSP__CRYPTO_FILE)) {
 } else {
     $crypto_file = fopen(MFSP__CRYPTO_FILE, "w") or die("Unable to open file!");
 
     $encrypt_method = MFSP__CRYPTO_CIPHER;
-    $secret_key = generateUUID4();
-    $secret_iv = generateUUID4();
+    $secret_key = generateUUID4Secret();
+    $secret_iv = generateUUID4Secret();
 
     $key = hash('sha256', $secret_key);
     $iv = substr(hash('sha256', $secret_iv), 0, 16);
